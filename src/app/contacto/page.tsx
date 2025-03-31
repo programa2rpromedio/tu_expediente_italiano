2266f84312bf1940897e4842df74a4e2064ea0cf
NeilenC <neilen.monlezun@nano-apps.com>
2025-03-31 12:07:14 -0300

    contacto + componente agradecimientos

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -6,23 +6,34 @@ import LinkeIn from "@/components/icon/LinkeIn";
 import Oficina from "@/components/icon/Oficina";
 import Telefono from "@/components/icon/Telefono";
 import Maps from "@/components/Maps";
+import ThanksForContacting from "@/components/ThanksForContacting";
 import React from "react";
 
 const Contacto = () => {
   return (
     <div>
-      <div className="bg-[#F6F6F6] h-auto">
 
-      <h2 className="text-text-green text-[28px] font-semibold text-center pt-[62px] pb-[16px]">¡Estás a un paso de tu Ciudadanía Italiana!</h2>
-      <p className="text-[24px] text-text-green text-center m-auto w-[350px] pb-[62px]">Agendá una asesoría con nosotros y evaluamos tu caso</p>
-
-      </div>
-
-      <section className='relative -full lg:h-[648px] bg-[url("/bg_form.webp")] bg-cover bg-center flex justify-center items-center'>
-        <div className="lg:w-[1035px] lg:h-[537px] flex items-center justify-center gap-[195px]">
-          <Form />
+      
+      {/* Contenedor de contacto  */}
+      <div>
+        <div className="bg-[#F6F6F6] h-auto">
+          <h2 className="text-text-green sm:text-[28px] text-[20px] w-[284px] sm:w-[100%] m-auto font-semibold text-center pt-[62px] pb-[16px]">
+            ¡Estás a un paso de tu Ciudadanía Italiana!
+          </h2>
+          <p className="sm:text-[24px] text-[16px] text-text-green text-center m-auto w-[350px] pb-[62px]">
+            Agendá una asesoría con nosotros y evaluamos tu caso
+          </p>
         </div>
-      </section>
+
+        <section className='relative -full lg:h-[648px] bg-[url("/bg_form.webp")] bg-cover bg-center flex justify-center items-center'>
+          <div className="bg-[url('/ruta-de-la-imagen.jpg')] bg-cover   sm:bg-center ">
+            <Form />
+          </div>
+        </section>
+      </div>
+      
+      {/* Agradecimientos por contactar */}
+      {/* <ThanksForContacting/> */}
 
       <div className="flex flex-col justify-center align-center text-center gap-[24px] py-[80px]">
         <h2 className="text-[28px] text-text-green font-bold">
@@ -36,25 +47,26 @@ const Contacto = () => {
         </div>
       </div>
 
-      <div className="  gap-[24px]  flex flex-row justify-center pb-[80px]">
-        <div className=" w-[356px] ">
+      <div className="gap-[24px]  flex flex-wrap sm:flex-row flex-col justify-center items-center sm:pb-[80px] pb-[24px]">
+        <div className="w-[356px] items-center flex justify-center">
           <CardInfo
             title="Nuestra oficina"
-            description="Calle 48, Num 877. La Plata. 
-          Buenos Aires, Argentina"
+            description="Calle 48, Num 877. La Plata. Buenos Aires, Argentina"
             icon={<Oficina className="text-primary-dark" />}
           />
         </div>
-        <div className=" w-[356px] ">
+        <div className="w-[356px] flex justify-center">
           <CardInfo
             title="Contacto"
-            description={`Teléfono: +54 9 1124084508 Mail: tuexpedienteitaliano@gmail.com`}
+            description={`Teléfono: +54 9 1124084508 Mail: tuexpedienteitaliano@gmail.com`}
             icon={<Telefono className="text-primary-dark" />}
           />
         </div>
       </div>
 
-      <Maps />
+      <div className="px-[20px] pb-[40px]">
+        <Maps />
+      </div>
     </div>
   );
 };
--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -2,12 +2,13 @@ import { CardInfoProps } from '@/lib/types';
 
 export default function CardInfo({ title, description, icon }: CardInfoProps) {
   return (
-    <div className='flex flex-col items-center gap-[10px] rounded-[10px] lg:py-[24px] lg:px-[16px] bg-primary-extra-light'>
-      {icon}
-      <h3 className='font-amaranth text-xl'>{title}</h3>
-      <p className='text-neutral-dark font-dm-sans text-[18px] text-center'>
-        {description}
-      </p>
-    </div>
+    <div className=' sm:w-[356px] w-[319px] h-[181px] flex flex-col justify-center items-center gap-[10px] rounded-[10px] lg:py-[24px] lg:px-[16px] bg-primary-extra-light'>
+  {icon}
+  <h3 className='font-amaranth text-xl text-center'>{title}</h3>
+  <p className='text-neutral-dark font-dm-sans text-[18px] text-center'>
+    {description}
+  </p>
+</div>
+
   );
 }
